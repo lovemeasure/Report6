2223c222d6f81f605a86dc6fbb7265895381f8bb
keito <e225746@ie.u-ryukyu.ac.jp>
2023-01-15 20:18:29 +0900

    ファイル作成

--- src/jp.ac.uryukyu.ie.e225746/Main.java
+++ src/jp.ac.uryukyu.ie.e225746/Main.java
@@ -1,2 +1,7 @@
+package jp.ac.uryukyu.ie.e225746;
+
 public class Main {
+    public static void main(String[] args){
+
+    }
 }
---
+++ src/jp.ac.uryukyu.ie.e225746/Score.java
@@ -0,0 +1,5 @@
+package jp.ac.uryukyu.ie.e225746;
+
+public class Score {
+    
+}
